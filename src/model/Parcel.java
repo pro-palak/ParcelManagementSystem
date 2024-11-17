50ddf57c6fb3de03a4114225eeb74715aa84d412
pro-palak <96881581+pro-palak@users.noreply.github.com>
2024-11-17 14:29:25 +0000

    Update

--- src/model/Parcel.java
+++ src/model/Parcel.java
@@ -1,5 +1,13 @@
 package model;
 
 public class Parcel {
-
+	    private String parcelID;
+	    private double weight;
+	    private int noOfDays;
+	    
+	    public Parcel(String parcelID, double weight, int noOfDays) {
+	        this.parcelID = parcelID;
+	        this.weight = weight;
+	        this.noOfDays = noOfDays;
+	    }
 }
